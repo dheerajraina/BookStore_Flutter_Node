d5a64f95ade95040e6f702632fd0a969b6e81b6b
Dheeraj Raina <dheerajraina55@gmail.com>
2023-07-30 15:58:16 +0530

    added test for getBookCover function in BookStoreServices

--- server/src/tests/bookStore.test.ts
+++ server/src/tests/bookStore.test.ts
@@ -1,9 +1,13 @@
-import BookStoreService from "../services/bookstore.service";
+import BookStoreServices from "../services/bookstore.services";
 describe("BookStore Services", () => {
-	const bookStoreServices = new BookStoreService();
+	const bookStoreServices = new BookStoreServices();
 
 	it("get books", async () => {
 		const result = await bookStoreServices.getBooks();
+		expect(result).resolves;
+	}, 10000);
+	it("get book cover", async () => {
+		const result = await bookStoreServices.getBookCover("8u394u93jcnjriw");
 		expect(result).resolves;
 	}, 10000);
 });
