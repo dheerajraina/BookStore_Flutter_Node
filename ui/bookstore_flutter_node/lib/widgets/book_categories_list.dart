fdddcddcbac87591f14cf5a03be595f5c95302a9
Dheeraj Raina <dheerajraina55@gmail.com>
2023-07-31 15:20:24 +0530

    widget to display books in form of different categories

---
+++ ui/bookstore_flutter_node/lib/widgets/book_categories_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:bookstore_flutter_node/config/config.dart';
+import 'widgets.dart';
+
+class BookCategories extends StatelessWidget {
+  const BookCategories({super.key,required this.bookList});
+  final bookList;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return SizedBox(
+      height: screenHeight,
+      width: screenWidth,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 100.0),
+        child: ListView.builder(
+            itemCount: BOOK_CATEGORIES.length,
+            itemBuilder: (context, index) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, right: 8.0, bottom: 8.0, top: 8.0),
+                    child: Text(
+                      BOOK_CATEGORIES[index],
+                      style: basicBoldTextStyle(context),
+                    ),
+                  ),
+                  BooksHorizontalDisplayList(booksList: [])
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
