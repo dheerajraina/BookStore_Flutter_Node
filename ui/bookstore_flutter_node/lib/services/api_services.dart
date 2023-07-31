282f905b89455c78bb6f08c8cea0f29e27bb353b
Dheeraj Raina <dheerajraina55@gmail.com>
2023-07-31 22:58:29 +0530

    added function for fetching book cover and book content

--- ui/bookstore_flutter_node/lib/services/api_services.dart
+++ ui/bookstore_flutter_node/lib/services/api_services.dart
@@ -11,6 +11,22 @@ class ApiServices {
   Future<bool> getBooks() async {
     var response = await ApiHelper().get("book-store/books");
     homePageController.bookList.value = bookModelFromJson(response.body);
+    homePageController.bookList.value.forEach((book) async {
+      book.data.coverUrl = await getBookCover(book.data.reference);
+    });
     return true;
+  }
+
+  Future<String> getBookCover(String bookReference) async {
+    var response = await ApiHelper().get("book-store/book-cover/$bookReference");
+    return response.body;
+  }
+
+  Future getBookContent(String bookReference) async {
+    homePageController.isLoading.value = true;
+    var response = await ApiHelper().get("book-store/book-content/$bookReference");
+    var pages = json.decode(response.body)['items'];
+    homePageController.isLoading.value = false;
+    return pages;
   }
 }
