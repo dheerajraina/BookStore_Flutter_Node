d6adde375128ae3693f6f6d94ae46c3fe717029c
Dheeraj Raina <dheerajraina55@gmail.com>
2023-07-31 15:08:54 +0530

    added reponsive layout for home page

---
+++ ui/bookstore_flutter_node/lib/responsive/responsive_home/responsive_home_layout.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveHomeLayout extends StatelessWidget {
+  final Widget mobileBody;
+  final Widget desktopBody;
+  ResponsiveHomeLayout({required this.mobileBody, required this.desktopBody});
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: ((context, constraints) {
+      if (constraints.maxWidth < 600) {
+        return mobileBody;
+      } else {
+        return desktopBody;
+      }
+    }));
+  }
+}
