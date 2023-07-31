c2072be23e400850a4c23dea2e41461c3b259edf
Dheeraj Raina <dheerajraina55@gmail.com>
2023-07-31 18:13:40 +0530

    added stateful wrapper for the purpose of using init fn

--- ui/bookstore_flutter_node/lib/pages/homepage.dart
+++ ui/bookstore_flutter_node/lib/pages/homepage.dart
@@ -1,12 +1,39 @@
+import 'package:bookstore_flutter_node/services/api_services.dart';
 import 'package:flutter/material.dart';
 import 'package:bookstore_flutter_node/responsive/responsive.dart';
+
+class StatefulWrapper extends StatefulWidget {
+  final Function onInit;
+  final Widget child;
+  const StatefulWrapper({required this.onInit, required this.child});
+  @override
+  _StatefulWrapperState createState() => _StatefulWrapperState();
+}
+
+class _StatefulWrapperState extends State<StatefulWrapper> {
+  @override
+  void initState() {
+    super.initState();
+    widget.onInit();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: ResponsiveHomeLayout(mobileBody: HomeMobileBody(), desktopBody: HomeDesktopBody()),
+      child: StatefulWrapper(
+        onInit: () {
+          ApiServices().getBooks();
+        },
+        child: Scaffold(
+          body: ResponsiveHomeLayout(mobileBody: HomeMobileBody(), desktopBody: HomeDesktopBody()),
+        ),
       ),
     );
   }
