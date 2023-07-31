26bdcdb50e290f1d244ff3e9b8a0c829889a6764
Dheeraj Raina <dheerajraina55@gmail.com>
2023-07-31 18:07:38 +0530

    added initializer function for controllers

--- ui/bookstore_flutter_node/lib/main.dart
+++ ui/bookstore_flutter_node/lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:bookstore_flutter_node/controllers/homepage_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'pages/pages.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
+  initializeController();
+
+}
+
+void initializeController() {
+  Get.put(HomePageController());
 }
 
 class MyApp extends StatelessWidget {
